1fa6417a6c404bacb7a5e37a1fc96b5ccf50be88
Scott Herbert <hscott@bluchip.co.ke>
2020-02-27 15:36:42 +0300

    "Updated file"

--- resources/js/app.js
+++ resources/js/app.js
@@ -107,16 +107,7 @@ Vue.component('vue-date-picker', VueCtkDateTimePicker);
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
 let routes = [
-    { path: '/', component: require('./components/Dashboard.vue').default},
-    { path: '/dashboard', component: require('./components/Dashboard.vue').default},
-    { path: '/users', component: require('./components/users/Index.vue').default},
-    { path: '/users/create', component: require('./components/users/Create.vue').default},
-    { path: '/roles', component: require('./components/roles/Roles.vue').default},
-    { path: '/trips', component: require('./components/trips/Index.vue').default},
-    { path: '/schedules', component: require('./components/schedules/Index.vue').default},
-    { path: '/schedules/closed', component: require('./components/schedules/Closed.vue').default},
-    { path: '/schedules/:id/show', component: require('./components/schedules/Trips.vue').default},
-    { path: '/roles/create', component: require('./components/roles/Create.vue').default},
+    
 
     // REAL ESTATE ROUTES
 
@@ -156,53 +147,8 @@ let routes = [
     { path: '/reports/tenants/:id/invoice', component: require('./components/tenants/Invoice.vue').default},
 
 
-    // END REAL ESTATE ROUTES
+    // END REAL ESTATE ROUTES   
 
-    { path: '/drivers', component: require('./components/drivers/Index.vue').default},
-    { path: '/driver/start/trip', component: require('./components/events/Trip.vue').default},
-    { path: '/driver/continue/trip', component: require('./components/events/editTrip.vue').default},
-    { path: '/drivers/create', component: require('./components/drivers/Create.vue').default},
-    { path: '/drivers/show/:id', component: require('./components/drivers/Show.vue').default},
-    { path: '/drivers/edit/:id', component: require('./components/drivers/Edit.vue').default},
-    { path: '/drivers/:id/vehicles', component: require('./components/drivers/Vehicles.vue').default},
-    { path: '/drivers/:id/trips', component: require('./components/drivers/Trips.vue').default},
-    { path: '/drivers/:id/collections', component: require('./components/drivers/Collections.vue').default},
-    { path: '/drivers/:id/commissions', component: require('./components/drivers/Commissions.vue').default},
-    { path: '/drivers/:id/schedules', component: require('./components/drivers/Schedules.vue').default},
-    { path: '/drivers/schedule/create', component: require('./components/schedules/Create.vue').default},
-    { path: '/drivers/:id/schedule/create', component: require('./components/schedules/Create.vue').default},
-    { path: '/bookings', component: require('./components/bookings/Index.vue').default},
-    { path: '/vehicles', component: require('./components/vehicles/Index.vue').default},
-    { path: '/vehicles/create', component: require('./components/vehicles/Create.vue').default},
-    { path: '/vehicles/show/:id', component: require('./components/vehicles/Show.vue').default},
-    { path: '/vehicles/edit/:id', component: require('./components/vehicles/Edit.vue').default},
-    { path: '/settings/general', component: require('./components/settings/General.vue').default},
-    { path: '/settings/structures', component: require('./components/structures/Index.vue').default},
-    { path: '/settings/structures/create', component: require('./components/structures/Create.vue').default},
-    { path: '/settings/structures/edit/:id', component: require('./components/structures/Edit.vue').default},
-    { path: '/settings/structures/show/:id', component: require('./components/structures/Edit.vue').default},
-    { path: '/organizations', component: require('./components/clients/Index.vue').default},
-    { path: '/organizations/:id/edit', component: require('./components/clients/Edit.vue').default},
-    { path: '/organizations/create', component: require('./components/clients/Create.vue').default},
-    { path: '/invoices', component: require('./components/invoices/Index.vue').default},
-    { path: '/reports', component: require('./components/reports/Index.vue').default},
-    { path: '/reports/drivers', component: require('./components/reports/Drivers.vue').default},
-    { path: '/reports/clients', component: require('./components/reports/clients/Index.vue').default},
-    // { path: '/reports/clients/:id/fm/:from/tn/:to', component: require('./components/windows/Clients.vue').default},
-    
-    // { path: '/reports/invoices', component: require('./components/reports/invoices/Index.vue').default},
-    // { path: '/reports/invoice/:id', component: require('./components/windows/Invoice.vue').default},
-    { path: '/reports/invoices/:id/fm/:from/tn/:to', component: require('./components/windows/List.vue').default},
-
-    { path: '/reports/commissions', component: require('./components/reports/drivers/commSummary.vue').default},
-    { path: '/reports/drivers/commissions', component: require('./components/reports/drivers/Comm.vue').default},
-
-    { path: '/reports/drivers/trips', component: require('./components/reports/drivers/Trips.vue').default},
-
-    { path: '/reports/worksheets', component: require('./components/reports/drivers/Schedule.vue').default},
-    { path: '/reports/worksheets/:id/fm/:from/tn/:to', component: require('./components/windows/Amounts.vue').default},
-
-    { path: '/reports/drivers/worksheet/:id', component: require('./components/windows/Schedule.vue').default},
   ]
 
 // 3. Create the router instance and pass the `routes` option
